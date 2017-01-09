6f4c996bbae54b394adc87fa8366e2e868604c1a
Evgeny Mandrikov <mandrikov@gmail.com>
2017-01-09 02:56:00 +0100

    (wip) reproducer for ArrayIndexOutOfBoundsException
    I had a hard time extracting it from ExpressionParser.

---
+++ org.jacoco.core.test/src/org/jacoco/core/test/validation/WIPTest.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2017 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation;
+
+import org.jacoco.core.test.validation.targets.WIP;
+import org.junit.Test;
+
+public class WIPTest extends ValidationTestBase {
+
+    public WIPTest() {
+        super(WIP.class);
+    }
+
+    @Test
+    public void run() {
+    }
+
+}
