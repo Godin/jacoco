d6b614d44acb9610bab35a5dfe00fba807502f9f
Evgeny Mandrikov <mandrikov@gmail.com>
2018-08-18 05:07:33 +0200

    (WIP) KotlinDelegationTarget

---
+++ org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/targets/KotlinDelegationTarget.kt
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2018 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.kotlin.targets
+
+import org.jacoco.core.test.validation.targets.Stubs.nop
+
+/**
+ * This test target is `???`.
+ */
+object KotlinDelegationTarget { // assertFullyCovered()
+
+    interface Base {
+        fun f()
+    }
+
+    class BaseImpl : Base { // assertFullyCovered()
+        override fun f() {
+            nop() // assertFullyCovered()
+        }
+    }
+
+    class Derived(b: Base) : Base by b // assertFullyCovered()
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val b = BaseImpl()
+        Derived(b).f()
+    }
+
+}
