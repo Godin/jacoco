7b150cf21f9c5f3ab50faa3682ffca5ed725acf8
Evgeny Mandrikov <mandrikov@gmail.com>
2019-02-05 02:08:10 +0100

    (WIP)

--- org.jacoco.core.test/src/org/jacoco/core/internal/instr/ProbeArrayStrategyFactoryTest.java
+++ org.jacoco.core.test/src/org/jacoco/core/internal/instr/ProbeArrayStrategyFactoryTest.java
@@ -12,23 +12,19 @@
 package org.jacoco.core.internal.instr;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
-
-import java.util.ArrayList;
-import java.util.List;
+import static org.junit.Assert.assertTrue;
 
 import org.jacoco.core.runtime.IExecutionDataAccessorGenerator;
-import org.jacoco.core.runtime.OfflineInstrumentationAccessGenerator;
-import org.junit.Before;
 import org.junit.Test;
 import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodNode;
 
 /**
  * Unit tests for {@link ProbeArrayStrategyFactory} and the
@@ -37,14 +33,17 @@ import org.objectweb.asm.Opcodes;
  */
 public class ProbeArrayStrategyFactoryTest {
 
-	private IExecutionDataAccessorGenerator generator;
-	private ClassVisitorMock cv;
+	private IExecutionDataAccessorGenerator generator = new IExecutionDataAccessorGenerator() {
+		public int generateDataAccessor(final long classid,
+				final String classname, final int probecount,
+				final MethodVisitor mv) {
+			assertEquals("Foo", classname);
+			assertEquals(42, classid);
+			return 0;
+		}
+	};
 
-	@Before
-	public void setup() {
-		generator = new OfflineInstrumentationAccessGenerator();
-		cv = new ClassVisitorMock();
-	}
+	private ClassNode c = new ClassNode();
 
 	@Test
 	public void testClass1() {
@@ -116,9 +115,6 @@ public class ProbeArrayStrategyFactoryTest {
 		assertEquals(ClassFieldProbeArrayStrategy.class, strategy.getClass());
 		assertDataField(InstrSupport.DATAFIELD_ACC);
 		assertInitMethod(true);
-
-		strategy.storeInstance(cv.visitMethod(0, null, null, null, null), false,
-				0);
 	}
 
 	@Test
@@ -148,16 +144,24 @@ public class ProbeArrayStrategyFactoryTest {
 
 	@Test
 	public void testInterface8() {
-		cv.isInterface = true;
 		final IProbeArrayStrategy strategy = test(Opcodes.V1_8,
 				Opcodes.ACC_INTERFACE, false, true, true);
 		assertEquals(InterfaceFieldProbeArrayStrategy.class,
 				strategy.getClass());
 		assertDataField(InstrSupport.DATAFIELD_INTF_ACC);
-		assertInitAndClinitMethods();
 
-		strategy.storeInstance(cv.visitMethod(0, null, null, null, null), false,
-				0);
+		assertEquals(2, c.methods.size());
+
+		MethodNode m = c.methods.get(c.methods.size() - 2);
+		assertEquals(InstrSupport.INITMETHOD_NAME, m.name);
+		assertEquals(InstrSupport.INITMETHOD_DESC, m.desc);
+		assertEquals(InstrSupport.INITMETHOD_ACC, m.access);
+		assertTrue(hasFrames(m));
+
+		m = c.methods.get(c.methods.size() - 1);
+		assertEquals(InstrSupport.CLINIT_NAME, m.name);
+		assertEquals(InstrSupport.CLINIT_DESC, m.desc);
+		assertEquals(InstrSupport.CLINIT_ACC, m.access);
 	}
 
 	@Test
@@ -183,9 +187,6 @@ public class ProbeArrayStrategyFactoryTest {
 		assertEquals(LocalProbeArrayStrategy.class, strategy.getClass());
 		assertNoDataField();
 		assertNoInitMethod();
-
-		strategy.storeInstance(cv.visitMethod(0, null, null, null, null), false,
-				0);
 	}
 
 	@Test
@@ -199,16 +200,12 @@ public class ProbeArrayStrategyFactoryTest {
 
 	@Test
 	public void testClinitAndMethodsInterface8() {
-		cv.isInterface = true;
 		final IProbeArrayStrategy strategy = test(Opcodes.V1_8,
 				Opcodes.ACC_INTERFACE, true, true, true);
 		assertEquals(InterfaceFieldProbeArrayStrategy.class,
 				strategy.getClass());
 		assertDataField(InstrSupport.DATAFIELD_INTF_ACC);
-		assertInitAndClinitMethods();
-
-		strategy.storeInstance(cv.visitMethod(0, "<clinit>", null, null, null),
-				true, 0);
+		assertInitMethod(true);
 	}
 
 	@Test
@@ -226,164 +223,73 @@ public class ProbeArrayStrategyFactoryTest {
 
 	private IProbeArrayStrategy test(int version, int access, boolean clinit,
 			boolean method, boolean abstractMethod) {
-		final ClassWriter writer = new ClassWriter(0);
-		writer.visit(version, access, "Foo", "java/lang/Object", null, null);
+		ClassNode c = new ClassNode();
+		c.version = version;
+		c.access = access;
+		c.name = "Foo";
 		if (clinit) {
-			final MethodVisitor mv = writer.visitMethod(
-					Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "<clinit>", "()V",
-					null, null);
-			mv.visitCode();
-			mv.visitInsn(Opcodes.RETURN);
-			mv.visitMaxs(0, 0);
-			mv.visitEnd();
+			MethodNode m = new MethodNode(0, "<clinit>", "()V", null, null);
+			m.instructions.add(new InsnNode(Opcodes.RETURN));
+			c.methods.add(m);
 		}
 		if (method) {
-			final MethodVisitor mv = writer.visitMethod(
-					Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "doit", "()V",
-					null, null);
-			mv.visitCode();
-			mv.visitInsn(Opcodes.RETURN);
-			mv.visitMaxs(0, 0);
-			mv.visitEnd();
+			MethodNode m = new MethodNode(0, "m", "()V", null, null);
+			m.instructions.add(new InsnNode(Opcodes.RETURN));
+			c.methods.add(m);
 		}
 		if (abstractMethod) {
-			final MethodVisitor mv = writer.visitMethod(Opcodes.ACC_ABSTRACT,
-					"foo", "()V", null, null);
-			mv.visitEnd();
+			MethodNode m = new MethodNode(Opcodes.ACC_ABSTRACT, "m", "()V",
+					null, null);
+			c.methods.add(m);
 		}
-		writer.visitEnd();
 
-		final IProbeArrayStrategy strategy = ProbeArrayStrategyFactory
-				.createFor(0, new ClassReader(writer.toByteArray()), generator);
+		final ClassWriter writer = new ClassWriter(0);
+		c.accept(writer);
 
-		strategy.addMembers(cv, 123);
+		IProbeArrayStrategy strategy = ProbeArrayStrategyFactory.createFor(42,
+				new ClassReader(writer.toByteArray()), generator);
+
+		for (MethodNode m : c.methods) {
+			strategy.storeInstance(m, "<clinit>".equals(m.name), 42);
+		}
+
+		strategy.addMembers(this.c, 42);
+
 		return strategy;
 	}
 
-	private static class AddedMethod {
-		private final int access;
-		private final String name;
-		private final String desc;
-		private boolean frames;
-
-		AddedMethod(int access, String name, String desc) {
-			this.access = access;
-			this.name = name;
-			this.desc = desc;
-		}
-
-		void assertInitMethod(boolean frames) {
-			assertEquals(InstrSupport.INITMETHOD_NAME, name);
-			assertEquals(InstrSupport.INITMETHOD_DESC, desc);
-			assertEquals(InstrSupport.INITMETHOD_ACC, access);
-			assertEquals(Boolean.valueOf(frames), Boolean.valueOf(frames));
-		}
-
-		void assertClinit() {
-			assertEquals(InstrSupport.CLINIT_NAME, name);
-			assertEquals(InstrSupport.CLINIT_DESC, desc);
-			assertEquals(InstrSupport.CLINIT_ACC, access);
-			assertEquals(Boolean.valueOf(false), Boolean.valueOf(frames));
-		}
-	}
-
-	private static class ClassVisitorMock extends ClassVisitor {
-
-		private boolean isInterface;
-
-		private int fieldAccess;
-		private String fieldName;
-		private final List<AddedMethod> methods = new ArrayList<AddedMethod>();
-
-		ClassVisitorMock() {
-			super(InstrSupport.ASM_API_VERSION);
-		}
-
-		@Override
-		public FieldVisitor visitField(int access, String name, String desc,
-				String signature, Object value) {
-			assertNull(fieldName);
-			fieldAccess = access;
-			fieldName = name;
-			return null;
-		}
-
-		@Override
-		public MethodVisitor visitMethod(int access, String name, String desc,
-				String signature, String[] exceptions) {
-			final AddedMethod m = new AddedMethod(access, name, desc);
-			methods.add(m);
-			return new MethodVisitor(InstrSupport.ASM_API_VERSION) {
-				@Override
-				public void visitFrame(int type, int nLocal, Object[] local,
-						int nStack, Object[] stack) {
-					m.frames = true;
-				}
-
-				@Override
-				public void visitFieldInsn(int opcode, String owner,
-						String name, String desc) {
-					assertEquals(InstrSupport.DATAFIELD_NAME, name);
-					assertEquals(InstrSupport.DATAFIELD_DESC, desc);
-
-					if (opcode == Opcodes.GETSTATIC) {
-						assertEquals(InstrSupport.INITMETHOD_NAME,
-								methods.get(methods.size() - 1).name);
-					} else if (opcode == Opcodes.PUTSTATIC) {
-						if (isInterface) {
-							assertEquals(InstrSupport.CLINIT_NAME,
-									methods.get(methods.size() - 1).name);
-						} else {
-							assertEquals(InstrSupport.INITMETHOD_NAME,
-									methods.get(methods.size() - 1).name);
-						}
-					} else {
-						fail();
-					}
-				}
-
-				@Override
-				public void visitMethodInsn(int opcode, String owner,
-						String name, String desc, boolean itf) {
-					if ("getProbes".equals(name)) {
-						// method's owner is not interface:
-						assertFalse(itf);
-						return;
-					}
-					assertEquals(Boolean.valueOf(itf),
-							Boolean.valueOf(isInterface));
-
-					assertEquals(Opcodes.INVOKESTATIC, opcode);
-					assertEquals("Foo", owner);
-					assertEquals(InstrSupport.INITMETHOD_NAME, name);
-					assertEquals(InstrSupport.INITMETHOD_DESC, desc);
-				}
-			};
-		}
-	}
-
 	void assertDataField(int access) {
-		assertEquals(InstrSupport.DATAFIELD_NAME, cv.fieldName);
-		assertEquals(access, cv.fieldAccess);
+		assertEquals(1, c.fields.size());
+		FieldNode f = c.fields.get(0);
+		assertEquals(InstrSupport.DATAFIELD_NAME, f.name);
+		assertEquals(access, f.access);
 	}
 
 	void assertNoDataField() {
-		assertNull(cv.fieldName);
+		assertEquals(0, c.fields.size());
 	}
 
 	void assertInitMethod(boolean frames) {
-		assertEquals(cv.methods.size(), 1);
-		cv.methods.get(0).assertInitMethod(frames);
-	}
-
-	void assertInitAndClinitMethods() {
-		assertEquals(2, cv.methods.size());
-		cv.methods.get(0).assertInitMethod(true);
-		cv.methods.get(1).assertClinit();
+		assertEquals(1, c.methods.size());
+		MethodNode m = c.methods.get(0);
+		assertEquals(InstrSupport.INITMETHOD_NAME, m.name);
+		assertEquals(InstrSupport.INITMETHOD_DESC, m.desc);
+		assertEquals(InstrSupport.INITMETHOD_ACC, m.access);
+		assertEquals(frames, hasFrames(m));
 	}
 
 	void assertNoInitMethod() {
-		assertEquals(0, cv.methods.size());
+		assertEquals(0, c.methods.size());
+	}
+
+	private static boolean hasFrames(MethodNode m) {
+		for (AbstractInsnNode i = m.instructions.getFirst(); i != null; i = i
+				.getNext()) {
+			if (i.getType() == AbstractInsnNode.FRAME) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
