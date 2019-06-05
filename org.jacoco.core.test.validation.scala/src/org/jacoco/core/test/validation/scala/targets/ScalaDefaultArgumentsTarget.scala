71883419d3b2cb179dd4b06316baa64755cee163
Evgeny Mandrikov <mandrikov@gmail.com>
2019-06-05 11:58:36 +0200

    (WIP)

---
+++ org.jacoco.core.test.validation.scala/src/org/jacoco/core/test/validation/scala/targets/ScalaDefaultArgumentsTarget.scala
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2019 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.scala.targets
+
+/**
+ * Test target for functions with default arguments.
+ */
+object ScalaDefaultArgumentsTarget {
+
+  def f(a: String = "a", b: String = "b"): Unit = { // assertFullyCovered()
+  }
+
+  def branches(a: Boolean)(b: String = if (a) "a" else "b"): Unit = { // assertFullyCovered(0, 2)
+  }
+
+  def main(args: Array[String]): Unit = {
+    f()
+
+    branches(a = false)()
+    branches(a = true)()
+  }
+
+}
