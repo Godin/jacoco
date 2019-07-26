c1b1cca6d9cf74752c69bae833cb7018ebb17bfb
Evgeny Mandrikov <mandrikov@gmail.com>
2019-07-26 22:54:06 +0200

    (WIP)

---
+++ org.jacoco.core.test.validation.groovy/src/org/jacoco/core/test/validation/groovy/GroovyEnumTest.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2019 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.groovy;
+
+import org.jacoco.core.test.validation.ValidationTestBase;
+import org.jacoco.core.test.validation.groovy.targets.GroovyEnumTarget;
+import org.junit.Test;
+
+/**
+ * TODO
+ */
+public class GroovyEnumTest extends ValidationTestBase {
+
+    public GroovyEnumTest() {
+        super(GroovyEnumTarget.class);
+    }
+
+    @Test
+    public void test_method_count() {
+        assertMethodCount(2);
+    }
+
+}
