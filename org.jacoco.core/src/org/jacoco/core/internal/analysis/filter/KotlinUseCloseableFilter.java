7bb822abf82f58387ed54873640aeed169c88972
Evgeny Mandrikov <mandrikov@gmail.com>
2020-02-04 11:04:08 +0100

    (WIP) implement

--- org.jacoco.core/src/org/jacoco/core/internal/analysis/filter/KotlinUseCloseableFilter.java
+++ org.jacoco.core/src/org/jacoco/core/internal/analysis/filter/KotlinUseCloseableFilter.java
@@ -12,7 +12,10 @@
  *******************************************************************************/
 package org.jacoco.core.internal.analysis.filter;
 
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TryCatchBlockNode;
 
 /**
  * TODO
@@ -22,6 +25,37 @@ final class KotlinUseCloseableFilter implements IFilter {
 	public void filter(final MethodNode methodNode,
 			final IFilterContext context, final IFilterOutput output) {
 		// TODO
+
+		final Matcher matcher = new Matcher();
+		for (TryCatchBlockNode t : methodNode.tryCatchBlocks) {
+			if ("java/lang/Throwable".equals(t.type)) {
+				matcher.match(t.handler, output);
+			}
+		}
+
+	}
+
+	private static class Matcher extends AbstractMatcher {
+		void match(final AbstractInsnNode start, final IFilterOutput output) {
+			cursor = start.getPrevious();
+			nextIsVar(Opcodes.ASTORE, "t1");
+			nextIsVar(Opcodes.ALOAD, "t1");
+			nextIsVar(Opcodes.ASTORE, "exception");
+			nextIsVar(Opcodes.ALOAD, "t1");
+			nextIs(Opcodes.ATHROW);
+
+			nextIsVar(Opcodes.ASTORE, "t2");
+			nextIsVar(Opcodes.ALOAD, "resource");
+			nextIsVar(Opcodes.ALOAD, "exception");
+			nextIsInvoke(Opcodes.INVOKESTATIC, "kotlin/io/CloseableKt",
+				"closeFinally", "(Ljava/io/Closeable;Ljava/lang/Throwable;)V");
+			nextIsVar(Opcodes.ALOAD, "t2");
+			nextIs(Opcodes.ATHROW);
+
+			if (cursor != null) {
+				output.ignore(start, cursor);
+			}
+		}
 	}
 
 }
