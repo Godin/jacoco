903e05bf79b5f735f997a35e569bad4ac596c43d
Evgeny Mandrikov <mandrikov@gmail.com>
2020-02-04 11:02:02 +0100

    (WIP) add validation test

---
+++ org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/targets/KotlinUseCloseableTarget.kt
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2020 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.kotlin.targets
+
+import org.jacoco.core.test.validation.targets.Stubs.*
+import java.io.ByteArrayOutputStream
+
+/**
+ * TODO
+ */
+object KotlinUseCloseableTarget {
+
+    private fun withoutException() {
+        ByteArrayOutputStream().use { // assertFullyCovered()
+            nop() // assertFullyCovered()
+        } // assertFullyCovered()
+    }
+
+    private fun withException() {
+        ByteArrayOutputStream().use { // assertPartlyCovered()
+            nop() // assertFullyCovered()
+            ex() // assertNotCovered()
+        } // assertNotCovered()
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        withoutException()
+
+        try {
+            withException()
+        } catch (e: StubException) {
+        }
+    }
+
+}
