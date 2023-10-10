53369080fd477a76acb45bfbdc2af4a8f89b297b
Evgeny Mandrikov <mandrikov@gmail.com>
2023-10-10 11:57:14 +0200

    (WIP) try K2 1.9.20-Beta
    Failed tests:
      execute_assertions_in_comments(org.jacoco.core.test.validation.kotlin.KotlinWhenExpressionTest): Instructions (KotlinWhenExpressionTarget.kt:67) expected:<[FUL]LY_COVERED> but was:<[PART]LY_COVERED>
      execute_assertions_in_comments(org.jacoco.core.test.validation.kotlin.KotlinLateinitTest): Instructions (KotlinLateinitTarget.kt:26) expected:<[FUL]LY_COVERED> but was:<[PART]LY_COVERED>

--- org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/targets/KotlinCoroutineTarget.kt
+++ org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/targets/KotlinCoroutineTarget.kt
@@ -20,7 +20,7 @@ import org.jacoco.core.test.validation.targets.Stubs.nop
  */
 object KotlinCoroutineTarget {
 
-    private suspend fun suspendingFunction() { // assertFullyCovered()
+    private suspend fun suspendingFunction() { // assertEmpty()
         anotherSuspendingFunction() // assertFullyCovered()
         nop() // assertFullyCovered()
     } // assertFullyCovered()
