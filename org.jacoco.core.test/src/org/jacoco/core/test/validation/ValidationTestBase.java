cdc603ae7e161827c576b49e014dc99afa4ebdfb
Evgeny Mandrikov <mandrikov@gmail.com>
2024-08-05 13:42:54 +0200

    Calculate line coverage for Kotlin inline functions

--- org.jacoco.core.test/src/org/jacoco/core/test/validation/ValidationTestBase.java
+++ org.jacoco.core.test/src/org/jacoco/core/test/validation/ValidationTestBase.java
@@ -166,7 +166,7 @@ public abstract class ValidationTestBase {
 	}
 
 	@Test
-	public void all_missed_instructions_should_have_line_number() {
+	public final void all_missed_instructions_should_have_line_number() {
 		CounterImpl c = CounterImpl.COUNTER_0_0;
 		for (Line line : source.getLines()) {
 			c = c.increment(line.getCoverage().getInstructionCounter());
@@ -178,7 +178,7 @@ public abstract class ValidationTestBase {
 	}
 
 	@Test
-	public void all_branches_should_have_line_number() {
+	public final void all_branches_should_have_line_number() {
 		CounterImpl c = CounterImpl.COUNTER_0_0;
 		for (Line line : source.getLines()) {
 			c = c.increment(line.getCoverage().getBranchCounter());
