786fc60df77c2dbcad381ef699cd1039f75c933b
Evgeny Mandrikov <mandrikov@gmail.com>
2024-08-08 16:46:26 +0200

    (WIP) Add validation test for synthetic accessor methods in Kotlin

---
+++ org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/targets/KotlinSyntheticAccessorsTarget.kt
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2024 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.kotlin.targets
+
+/**
+ * Test target with synthetic accessor methods.
+ */
+object KotlinSyntheticAccessorsTarget {
+
+    class Outer { // assertFullyCovered()
+        private var x = 42
+
+        private fun getX(): Int {
+            return x
+        }
+
+        /**
+         * TODO add comments
+         */
+        inner class Inner {
+            fun accessOuter() {
+                x += 1 // assertNotCovered()
+                getX()
+            }
+        }
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Outer().Inner()
+    }
+
+}
