0aa1c9c164b25f6b9c6e3f6d59d0840d6811c350
Evgeny Mandrikov <mandrikov@gmail.com>
2026-06-11 22:18:59 +0200

    (WIP) Add validation test for JEP 401: Value Classes and Objects (Preview)

---
+++ org.jacoco.core.test.validation.java28/src/org/jacoco/core/test/validation/java28/targets/JEP401Target.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2026 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.java28.targets;
+
+import static org.jacoco.core.test.validation.targets.Stubs.nop;
+
+/**
+ * <a href="https://openjdk.org/jeps/401">JEP 401</a>)
+ */
+public class JEP401Target {
+
+	private value record R(int x) { // assertFullyCovered()
+	} // assertEmpty()
+
+	private static value class C { // assertEmpty()
+		private int x; // assertEmpty()
+		C(int x) { // assertFullyCovered()
+			this.x = x; // assertFullyCovered()
+		} // assertFullyCovered()
+	} // assertEmpty()
+
+	public static void main(String[] args) {
+		new R(1);
+		new C(1);
+	}
+
+}
